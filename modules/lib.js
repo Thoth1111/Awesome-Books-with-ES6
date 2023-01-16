36472868997f70f1e500bb697c905a1093e76413
Alwyn Androvelle <alwynsimiyu@gmail.com>
2023-01-16 15:52:16 +0300

    Add three module scripts

---
+++ modules/lib.js
@@ -0,0 +1,59 @@
+const storedBooks = JSON.parse(localStorage.getItem('bookstored'));
+const empty = document.querySelector('.empty_bookstore');
+const tableBody = document.querySelector('.table_body');
+
+const bookDetails = class Book {
+    constructor(title, author) {
+      this.title = title;
+      this.author = author;
+    }
+  }
+
+const BookActions = class AwesomeLibrary {
+    constructor() {
+      this.storedBooks = storedBooks;
+    }
+  
+    addBookDetails() {
+      const booksData = new bookDetails(title.value, author.value);
+      if (!this.storedBooks) {
+        this.storedBooks = [];
+      }
+      this.storedBooks.push(booksData);
+      localStorage.setItem('bookstored', JSON.stringify(this.storedBooks));
+      this.displayBooks();
+    }
+  
+    displayBooks() {
+      if (!this.storedBooks || !this.storedBooks === 0 || this.storedBooks === []) {
+        empty.innerHTML = 'No books in the library';
+      } else {
+        empty.style.display = 'none';
+        let eachTableRow = '';
+        for (let i = 0; i < this.storedBooks.length; i += 1) {
+          eachTableRow
+            += `<tr>
+                  <td>"${this.storedBooks[i].title}" by ${this.storedBooks[i].author}</td>
+                  <td><button type="button" id=${i} class="remove_btn">Remove</button></td>
+              </tr>
+              `;
+        }
+        tableBody.innerHTML = eachTableRow;
+        const deleteBookBtn = document.querySelectorAll('.remove_btn');
+        this.removeBooks(deleteBookBtn);
+      }
+    }
+  
+    removeBooks(deleteBookBtn) {
+      deleteBookBtn.forEach((elementBtn) => {
+        elementBtn.addEventListener('click', () => {
+          const index = elementBtn.getAttribute('id');
+          this.storedBooks.splice(index, 1);
+          localStorage.setItem('bookstored', JSON.stringify(this.storedBooks));
+          this.displayBooks();
+        });
+      });
+    }
+}
+
+export {BookActions};
